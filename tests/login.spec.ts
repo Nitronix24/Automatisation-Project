8f676ffdc30d343b8150666c13a9d8d4fd33a26d
collin <lemogo.collins21@myiuc.com>
2024-10-29 15:37:03 +0100

    Ajout des tests Login et Sign_up valider par le professeur

---
+++ tests/login.spec.ts
@@ -0,0 +1,12 @@
+
+import { test} from "./login_object/fixtures";
+
+test.beforeEach(async({page}) => {
+    await page.goto('https://www.amazon.fr/');
+    await page.getByLabel('Accepter').click();
+  }
+);
+
+test('login', async ({ login }) => { 
+    await login.login1();
+});
