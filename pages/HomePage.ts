5a15be806f86a423f6694e5babceda968bcf4ddd
Nitronix12 <antonin.ledee@student.junia.com>
2024-11-04 02:05:39 +0100

    Revert "Merge branch 'antonin'"
    This reverts commit be3ba6f045f523eb9c7711465b3989d878626924, reversing
    changes made to 1977e0cfdcbb5bebf023058d150aa1f50c203575.

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -12,38 +12,28 @@ export class HomePage {
     readonly kitchen_and_house: Locator;
     readonly gros_electromenager: Locator;
 
-  readonly logo: Locator;
-  readonly nav_search: Locator;
-  readonly best_sellers: Locator;
-  readonly vente_flash: Locator;
+    constructor(page : Page) {
+        this.page = page;
+        this.searchBox = page.locator('//*[@id="twotabsearchtextbox"]');
+        this.searchButton = page.locator('//*[@id="nav-search-submit-button"]');
+        this.cartLink = page.locator('//*[@id="nav-cart-count-container"]');
+        this.language = page.locator('#icp-nav-flyout');
+        this.change_language = page.locator('#icp-language-translation-hint');
+        this.hamburger_menu = page.locator('#nav-hamburger-menu');
+        this.viewMore = page.getByRole('link', { name: 'Tout afficher', class: 'hmenu-compressed-btn' });
+        this.kitchen_and_house = page.getByRole('link', { name: 'Cuisine et maison' });
+        this.gros_electromenager = page.locator('a[href="/gp/browse.html?node=57695031&ref_=nav_em__groselectro_0_2_15_14"]:visible');
 
-  constructor(page: Page) {
-    this.page = page;
-    this.searchBox = page.locator('//*[@id="twotabsearchtextbox"]'); // Sélecteur pour la barre de recherche
-    this.searchButton = page.locator('//*[@id="nav-search-submit-button"]'); // Sélecteur pour le bouton de recherche
-    this.cartLink = page.locator('//*[@id="nav-cart-count-container"]'); // Sélecteur pour le bouton du panier
-
-    this.language = page.locator('#icp-nav-flyout');
-    this.change_language = page.locator('#icp-language-translation-hint');
-    this.hamburger_menu = page.locator('#nav-hamburger-menu')
-    this.viewMore = page.getByRole('link', { name: 'Tout afficher', class: 'hmenu-compressed-btn' });
-    this.kitchen_and_house = page.locator('a[href="/cuisine-maison-%C3%A9lectrom%C3%A9nager/b/?ie=UTF8&node=57004031&ref_=nav_cs_home"]');
-    this.gros_electromenager = page.getByRole('link', { name: 'Gros électroménager' })
-
-    this.logo = page.locator('#nav-logo-sprites'); // Sélecteur pour le logo
-    this.nav_search = page.locator('#twotabsearchtextbox'); // Sélecteur pour la barre de recherche
-    this.best_sellers = page.getByLabel('navigation').getByRole('link', { name: 'Meilleures ventes' }); // Sélecteur pour le bouton meilleurs ventes
-    this.vente_flash = page.getByRole('link', { name: 'Ventes Flash' }); // Sélecteur pour le bouton ventes flash
-  }
+        }
+        
+    async searchForProduct(productName: string) {
+        await this.searchBox.fill(productName);
+        await this.searchButton.click();
+    }
     
-  async searchForProduct(productName: string) {
-    await this.searchBox.fill(productName);
-    await this.searchButton.click();
-  }
-  async goToCart() {
-    await this.cartLink.click();
-  }
-
+    async goToCart() {
+        await this.cartLink.click();
+    }
 
     async languageClick(){
         await this.language.click();
